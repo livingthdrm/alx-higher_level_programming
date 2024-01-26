30af39edfe7dc8108aca8749024ff5d7545415fe
livingthdrm <salaonjoseph@gmail.com>
2024-01-26 13:49:03 -0800

    fix 101

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    str1 = ""
+    for letter in str:
+        if letter != str[n]:
+            str1 += "{}".format(letter)
+    print("{}".format(str1))
