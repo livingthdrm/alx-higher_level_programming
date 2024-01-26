dade3ca11efd0a44660ca856304ac8c64c1e30fb
livingthdrm <salaonjoseph@gmail.com>
2024-01-26 13:07:45 -0800

    fix last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        print("{}".format(abs(number) % 10))
+        return abs(number) % 10
+        print("{}".format(abs(number) % 10), end="")
     else:
-        print("{}".format(number % 10))
-    return number % 10
+        print("{}".format(number % 10), end="")
+        return number % 10
