431ef8031d62cf9aba75c2f30218a13340a9ff97
livingthdrm <salaonjoseph@gmail.com>
2024-01-26 14:10:35 -0800

    fix 102

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    if a < b:
+        return True
+    else:
+        return False
+    if c > b:
+        return True
+    else:
+        return False
+
+    return a + b
+    return a * b - c
