af1265d79b42802397e7c8c5b49be7c468fec4b6
livingthdrm <salaonjoseph@gmail.com>
2024-02-06 12:27:01 -0800

    fix 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,5 +2,5 @@
 from add_0 import add
 a = 1
 b = 2
-result = add(a, b)
-print("{a:d} + {b:d} = {add(a, b)}".format(a, b, result))
+result
+print("{a:D} + {b:D} = {add(a, b)>:D}".format(a, b, (result=add_0.add(a, b))))
