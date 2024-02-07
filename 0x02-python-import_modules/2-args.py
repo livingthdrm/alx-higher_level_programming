8c5288b821b6dac446ed636be914c62358772448
livingthdrm <salaonjoseph@gmail.com>
2024-02-07 14:31:20 -0800

    fix 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,4 +2,13 @@
 """ a program that prints the number of and the list of its arguments """
 
 if __name__ == '__main__':
-
+    def prog(*argv):
+        for item in argv:
+            if len(argv) == 1:
+                print("0 arguments.")
+            elif len(argv) == 2:
+                print("1 argument:")
+                print("{}: {}".format((item + 1), argv(item + 1)))
+            else:
+                print("{} argument:".format(len(argv) - 1))
+                print("{}: {}".format((item + 1), argv(item + 1)))
