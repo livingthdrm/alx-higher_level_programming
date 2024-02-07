0a4ef8c128859e76c847de86c63d8ceb65968c8e
livingthdrm <salaonjoseph@gmail.com>
2024-02-06 23:46:42 -0800

    fix 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This program imports a module then utilizes it"""
-if __name__ = "__main__":
+if __name__ == "__main__":
     from add_0 import add
-        a = 1
-        b = 2
-        result = add(a, b)
-        print("{} + {} = {}".format(a, b, result))
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
