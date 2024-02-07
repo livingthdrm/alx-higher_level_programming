10da3c2392a4feb132ede5f2f43a2f079f0a832b
livingthdrm <salaonjoseph@gmail.com>
2024-02-06 23:43:01 -0800

    fix 0 by adding import guard?

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This program imports a module then utilizes it"""
-
-from add_0 import add
-a = 1
-b = 2
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+if __name__ = "__main__":
+    from add_0 import add
+        a = 1
+        b = 2
+        result = add(a, b)
+        print("{} + {} = {}".format(a, b, result))
