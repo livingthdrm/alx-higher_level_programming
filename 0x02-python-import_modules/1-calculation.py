3be07e4e961dbdaf9b3540dc4a114b432e619144
livingthdrm <salaonjoseph@gmail.com>
2024-02-07 00:17:10 -0800

    fix 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" a program that imports functions from the file calculator_1.py, 
+""" a program that imports functions from the file calculator_1.py,
 does some Maths, and prints the result """
 if __name__ == "__main__":
-    from calculator_1 import add , sub, mul, div
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
