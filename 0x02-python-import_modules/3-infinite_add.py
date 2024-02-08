1cb6014d3af1c520b10bd57e6df3ea3a60fdf572
livingthdrm <salaonjoseph@gmail.com>
2024-02-08 04:11:07 -0800

    fix 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" a program that prints the result of the addition of all arguments """
+
+import sys
+
+
+def mazematics(*argv):
+    result = 0
+    for i, item in enumerate(argv, start=1):
+        result += int(argv[item])
+        print("{}".format(result))
+
+
+if __name__ == "__main__":
+    mazematics(*sys.argv[1:])
