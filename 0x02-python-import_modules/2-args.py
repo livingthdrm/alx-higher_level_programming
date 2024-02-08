9f0f76181c7307cf39464c023f72c93c821ef2f0
livingthdrm <salaonjoseph@gmail.com>
2024-02-08 00:55:16 -0800

    fix 2 args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 """ a program that prints the number of and the list of its arguments """
 
-if __name__ == '__main__':
-    def prog(*argv):
-        for item in argv:
-            if len(argv) == 1:
-                print("0 arguments.")
-            elif len(argv) == 2:
-                print("1 argument:")
-                print("{}: {}".format((item + 1), argv(item + 1)))
-            else:
-                print("{} argument:".format(len(argv) - 1))
-                print("{}: {}".format((item + 1), argv(item + 1)))
+import sys
+
+
+def prog(*argv):
+    if len(argv) == 0:
+        print("0 arguments.")
+    elif len(argv) == 1):
+        print("1 argument:")
+        print("1: {}".format(argv(0)))
+    else:
+        print("{} argument:".format(len(argv)))
+        
+        for i, item in enumerate(argv, start = 1):
+            print("{}: {}".format(i, item))
+
+if __name == "__main__":
+    prog(*sys.argv[1:])
