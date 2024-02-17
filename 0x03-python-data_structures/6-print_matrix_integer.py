dabd045a8a5803f21bb90e15cd85b08ac38e49bf
livingthdrm <salaonjoseph@gmail.com>
2024-02-17 13:28:26 -0800

    fix natrix reader

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for index,  item in enumerate(row):
             if index == (len(row) - 1):
-                print(end="")
-            print("{:d}".format(item), end=" ")
+                print("{:d}".format(item), end="")
+            else:
+                print("{:d}".format(item), end=" ")
         print()
-    
