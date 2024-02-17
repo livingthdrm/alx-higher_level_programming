7532cbf1a879494e6ad29bdc6780b13b7cfd350c
livingthdrm <salaonjoseph@gmail.com>
2024-02-17 13:22:01 -0800

    attempt at printign a matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" a function that prints a matrix of integers. """
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for index,  item in enumerate(row):
+            if index == (len(row) - 1):
+                print(end="")
+            print("{:d}".format(item), end=" ")
+        print()
+    
