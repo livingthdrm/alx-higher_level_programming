4c50a5519c07707c918836663df5c5d6e327a6de
livingthdrm <salaonjoseph@gmail.com>
2024-02-19 02:16:59 -0800

    a few tweaks to set

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,9 +3,10 @@
 
 
 def common_elements(set_1, set_2):
-    common_set = set()
-    for element1 in set_1:
-        for element2 in set_2:
-            if element1 == element2:
-                common_set.add(element1)
-    return common_set
+    #common_set = set()
+    #for element1 in set_1:
+        #for element2 in set_2:
+           #if element1 == element2:
+                #common_set.add(element1)
+    #return common_set
+    return set(set_1 & set_2)
