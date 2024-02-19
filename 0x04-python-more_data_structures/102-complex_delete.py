8c098b6a5dffc746cd30f23d7dac7fdc2387bd8b
livingthdrm <salaonjoseph@gmail.com>
2024-02-19 13:12:58 -0800

    fix complex delete

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -6,7 +6,13 @@ def complex_delete(a_dictionary, value):
     for key, valued in a_dictionary.items():
         if value not in a_dictionary.values():
             return a_dictionary
-        else:
-            if value == valued:
-                del a_dictionary[key]
-                return complex_delete(a_dictionary, value)
+
+    keys_to_delete = [key for key, val in a_dictionary.items() if val == value]
+
+    for key in keys_to_delete:
+        del a_dictionary[key]
+
+    if value in a_dictionary.values():
+        complex_delete(a_dictionary, value)
+
+    return a_dictionary
