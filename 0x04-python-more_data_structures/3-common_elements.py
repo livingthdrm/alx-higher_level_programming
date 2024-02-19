e806efa8dbbdfac01f3bc4e3845e8b02a436d708
livingthdrm <salaonjoseph@gmail.com>
2024-02-19 01:54:09 -0800

    fix common set

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" a function that returns a set of common elements in two sets. """
+
+
+def common_elements(set_1, set_2):
+    common_set = set()
+    for element1 in set_1:
+        for element2 in set_2:
+            if element1 == element2:
+                common_set.add(element1)
+    return common_set
