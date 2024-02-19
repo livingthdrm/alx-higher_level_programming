7bc2f4b8acaf604e97e7d4b8acf98513027cacb9
livingthdrm <salaonjoseph@gmail.com>
2024-02-19 02:55:10 -0800

    fix sorted dict

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,4 @@
 
 def print_sorted_dictionary(a_dictionary):
     for keys, values in sorted(a_dictionary.items()):
-        print("{} : {}".format(keys, values))
+        print("{}: {}".format(keys, values))
