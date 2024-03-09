616335b24bcec34c410b6fb53c9598928396086e
livingthdrm <salaonjoseph@gmail.com>
2024-03-09 04:29:49 -0800

    remove whitespace from safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             if i < x:
                 print(item, end='')
         print()
-        return i + 1 
+        return i + 1
     except IndexError:
         return 1
