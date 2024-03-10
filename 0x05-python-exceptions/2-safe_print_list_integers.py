565e026eaabdd2caec9ddf073281656ecd953976
livingthdrm <salaonjoseph@gmail.com>
2024-03-10 04:10:47 -0700

    push safe int print

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" a function that prints the first x elements of a list and only integers."""
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+        for item in my_list[:x]:
+            if type(item) == int:
+                print("{:d}".format(item), end="")
+                count += 1
+        print()
+        return count
+    except IndexError:
+        print("list index out of range")
