56cc852e15afc9735997059728cdb16c7eb86cbf
livingthdrm <salaonjoseph@gmail.com>
2024-03-21 13:50:41 -0700

    my first class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" an empty class
+Square that defines a square """
+
+
+class Square:
+    """ This is a new class with no definitions yet
+
+    I'll use a pass statement to make it run """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+0x06. Python - Classes and Objects
+We write classes here
+__init__
