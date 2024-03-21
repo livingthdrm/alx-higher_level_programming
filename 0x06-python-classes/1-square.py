a08bfa83b258d44857e6ddb3d4eb3818ecfa1618
livingthdrm <salaonjoseph@gmail.com>
2024-03-21 13:55:02 -0700

    update class square with __size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" an class
+Square that defines a square """
+
+
+class Square:
+    """ a class Square that defines a square by: (based on 0-square.py)
+
+    Private instance attribute: size
+    Instantiation with size (no type/value verification) """
+
+    __size = 0
