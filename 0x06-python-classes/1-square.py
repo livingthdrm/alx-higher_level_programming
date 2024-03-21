c6b11f6102bfab2b06b2a9ac02e5ec909962aa44
livingthdrm <salaonjoseph@gmail.com>
2024-03-21 14:01:56 -0700

    instantiate class square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -10,3 +10,7 @@ class Square:
     Instantiation with size (no type/value verification) """
 
     __size = 0
+
+    def __init__(self, size):
+        """ A method to initialize square """
+        self.__size = size
