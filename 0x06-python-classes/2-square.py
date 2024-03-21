9ceb0ca32f92b3d294569543293e0fbb9fac920c
livingthdrm <salaonjoseph@gmail.com>
2024-03-21 14:09:56 -0700

    add exception handling to the class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" an class
+Square that defines a square """
+
+
+class Square:
+    """ a class Square that defines a square by: (based on 0-square.py)
+
+    Private instance attribute: size
+    Instantiation with size (no type/value verification) """
+
+    __size = 0
+
+    def __init__(self, size=0):
+        """ A method to initialize square
+            here we also try handle type error and value error
+            exceptions. """
+        try:
+            self.__size = size
+
+            if type(size) != int:
+                except TypeError:
+                    print("size must be an integer")
+            elif size < 0:
+                except ValueError:
+                    print("size must be >= 0")
