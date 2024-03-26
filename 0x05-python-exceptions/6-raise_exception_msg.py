74e56dff3d78fd7d8dfe2864c395ed3738bd9038
livingthdrm <salaonjoseph@gmail.com>
2024-03-26 01:33:15 -0700

    raise exception message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a function that raises a name exception with a message"""
+
+
+def raise_exception_msg(message=""):
+    try:
+        raise NameError
+    except NameError:
+        print(message)
