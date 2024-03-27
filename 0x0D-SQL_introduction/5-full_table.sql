bb5c359ce823cb5899d5d71ea9d8bffec03a758f
livingthdrm <salaonjoseph@gmail.com>
2024-03-26 17:00:52 -0700

    describe table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- a script that prints the full description of the table first_table
 --  from the database hbtn_0c_0 in your MySQL server
 SELECT hbtn_0c_0;
-SELECT * FROM first_table;
+DESC first_table;
