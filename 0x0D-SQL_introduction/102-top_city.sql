fc51ea2bf1cf68dc27c31728db1fdfeb2262aa6a
livingthdrm <salaonjoseph@gmail.com>
2024-03-29 03:53:57 -0700

    fix limit

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -3,6 +3,6 @@
 -- and August ordered by temperature (descending)
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-WHERE month = 7, 8
+WHERE month IN (7, 8)
 GROUP BY city
-ORDER BY avg_temp DESC;
+ORDER BY avg_temp DESC LIMIT 3;
