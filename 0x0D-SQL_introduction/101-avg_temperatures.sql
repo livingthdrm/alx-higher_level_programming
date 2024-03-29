f8edde139d2f98e0def5ce5ffaed8d9b1e2b441f
livingthdrm <salaonjoseph@gmail.com>
2024-03-29 03:43:01 -0700

    fix sqldump

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,4 @@
 -- Import in hbtn_0c_0 database this table dump: download
-mysql hbtn_0c_0 < temperatures.sql
-USE hbtn_0c_0;
-SELECT city, AVG(temperature) AS avg_temp
-ORDER BY temperature DESC;
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+ORDER BY value DESC;
