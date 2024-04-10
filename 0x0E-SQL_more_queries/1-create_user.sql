3c819e33b773325d4994d78b5c28e5c769c44fcc
livingthdrm <salaonjoseph@gmail.com>
2024-04-10 14:07:17 -0700

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,4 +3,4 @@
 -- The user_0d_1 password should be set to user_0d_1_pwd
 
 CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO user_0d_1;
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
