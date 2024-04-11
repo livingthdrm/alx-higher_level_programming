5aa3b2c14e09eabae737aef7750db9a8200ae027
livingthdrm <salaonjoseph@gmail.com>
2024-04-11 02:08:25 -0700

    insrt space on comment

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
---a script that creates the table force_name on your MySQL server.
+-- a script that creates the table force_name on your MySQL server.
 -- force_name description:
 -- id INT
 -- name VARCHAR(256) can’t be null
