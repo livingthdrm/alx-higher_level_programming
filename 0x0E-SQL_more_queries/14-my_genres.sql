fd757531681bdb07d698a87145832a6075263865
livingthdrm <salaonjoseph@gmail.com>
2024-04-15 06:57:17 -0700

    fix tv shows genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -12,4 +12,4 @@ INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title IN Dexter;
+WHERE tv_shows.title = 'Dexter';
