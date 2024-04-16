7db5045f16f0be3eca3e327c72dec31d8234b7ef
livingthdrm <salaonjoseph@gmail.com>
2024-04-16 13:56:46 -0700

    more inner joins

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -8,10 +8,11 @@
 
 SELECT tv_shows.title
 FROM tv_shows
-WHERE NOT IN (
+WHERE tv_shows.id NOT IN (
 	SELECT tv_show_genres.show_id
+	FROM tv_show_genres
 	INNER JOIN tv_genres
 	ON tv_show_genres.genre_id = tv_genres.id
 	WHERE tv_genre = 'Comedy'
 )
-ORDER BY  tv_shows.title ASC;
+ORDER BY tv_show.title ASC;
