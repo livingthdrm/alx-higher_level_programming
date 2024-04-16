578fa7b6e1d9e6e8fb0db602b490ac02d75183e3
livingthdrm <salaonjoseph@gmail.com>
2024-04-16 10:19:26 -0700

    shows not in dexter

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -8,9 +8,11 @@
 
 SELECT tv_genres.name
 FROM tv_genres
-LEFT JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows
-ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.name != 'Dexter'
-ORDER tv_genres.name ASC;
+WHERE tv_genres.id NOT IN (
+	SELECT tv_show_genres.genre_id
+	FROM tv_show_genres
+	INNER JOIN tv_shows
+	ON tv_shows.id = tv_show_genres.show_id
+	WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name ASC;
