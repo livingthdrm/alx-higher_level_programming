7de1aadf87261cdfe9cda671c643c26393a5859b
livingthdrm <salaonjoseph@gmail.com>
2024-04-19 16:27:50 -0700

    square method

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,20 +5,21 @@ square by: (based on 1-square.py) """
 
 class Square:
     """ Private instance attribute: size
-    You are not allowed to import any module"""
+    
+    Attributes:
+        size of the square"""
 
-    __size = 0
 
-    """ The initialization method """
     def __init__(self, size=0):
         """ Instantiation with optional size: def __init__(self, size=0):
             size must be an integer, otherwise raise a TypeError exception
             with the message size must be an integer
 
+            Args:
+                if size is less than 0, raise a ValueError exception
+                with the message size must be >= 0"""
 
-            if size is less than 0, raise a ValueError exception
-            with the message size must be >= 0"""
-
+        self.__size = size
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
