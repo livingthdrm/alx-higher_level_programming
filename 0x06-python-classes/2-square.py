45e02f7f6c1070e6dbf6b2a9f2524d6d341e49f2
livingthdrm <salaonjoseph@gmail.com>
2024-04-19 16:32:20 -0700

    fix blank lines

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-""" a class Square that defines a 
+""" a class Square that defines a
 square by: (based on 1-square.py) """
 
 
 class Square:
     """ Private instance attribute: size
-    
+
     Attributes:
         size of the square"""
-
-
     def __init__(self, size=0):
         """ Instantiation with optional size: def __init__(self, size=0):
             size must be an integer, otherwise raise a TypeError exception
@@ -19,7 +17,6 @@ class Square:
                 if size is less than 0, raise a ValueError exception
                 with the message size must be >= 0"""
 
-        self.__size = size
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
