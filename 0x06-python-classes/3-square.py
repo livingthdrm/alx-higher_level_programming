dafa831edf633c4a03de88050d29c306b144d4ba
livingthdrm <salaonjoseph@gmail.com>
2024-04-20 11:18:02 -0700

    find area of square using class

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,14 +7,14 @@ class Square:
     """ Private instance attribute: size
 
     Attributes:
-        size of the square"""
+        size of the square """
     def __init__(self, size=0):
-        """size must be an integer, otherwise raise a TypeError exception
+        """ size must be an integer, otherwise raise a TypeError exception
             with the message size must be an integer
 
             Args:
                 if size is less than 0, raise a ValueError exception
-                with the message size must be >= 0"""
+                with the message size must be >= 0 """
 
         if type(size) != int:
             raise TypeError("size must be an integer")
@@ -22,3 +22,8 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = size
+
+    def area(self):
+        """ A module that takes the size
+        and returns the current square area """
+        return self.__size * self.__size
