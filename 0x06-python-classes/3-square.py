10abd4744721b1cd87b0646aedbbb1bad532f94d
livingthdrm <salaonjoseph@gmail.com>
2024-04-21 03:51:15 -0700

    replace type with isinstance

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -16,7 +16,7 @@ class Square:
                 if size is less than 0, raise a ValueError exception
                 with the message size must be >= 0 """
 
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
