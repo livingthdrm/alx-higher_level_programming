031562a29868f63b7e14a9f9849f5f51018a0cdf
livingthdrm <salaonjoseph@gmail.com>
2024-04-23 15:52:07 -0700

    not a number

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* a script that prints My number: <first argument converted in
+ * integer> if the first argument can be converted to an integer */
+
+if (!(process.argv[2])) {
+	console.log('Not a number');
+}
+else if (parseInt(process.argv[2])) {
+	console.log('My number: ' + parseInt(process.argv[2]));
+}
+else {
+	console.log('Not a number');
+}
