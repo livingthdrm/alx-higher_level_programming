c946b78eac72cb0a91da182bfb0f4c3a557ec06b
livingthdrm <salaonjoseph@gmail.com>
2024-04-23 13:01:56 -0700

    fix argv

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/* a script that prints a message depending of the 
+ * numbe rod arguments passed */
+
+const { argv } = require('node:process');
+if process.argv.length == 2 {
+	console.log('No argument');
+}
+else if process.argv.length == 3 {
+	console.log('Argument found');
+}
+else {
+	console.log('Arguments found');
+}
