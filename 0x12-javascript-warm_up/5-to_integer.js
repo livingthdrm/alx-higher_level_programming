29408bb2617f1cb794a0154adf497a762a929a3f
livingthdrm <salaonjoseph@gmail.com>
2024-04-23 15:53:48 -0700

    used semistandard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,11 +3,9 @@
  * integer> if the first argument can be converted to an integer */
 
 if (!(process.argv[2])) {
-	console.log('Not a number');
-}
-else if (parseInt(process.argv[2])) {
-	console.log('My number: ' + parseInt(process.argv[2]));
-}
-else {
-	console.log('Not a number');
+  console.log('Not a number');
+} else if (parseInt(process.argv[2])) {
+  console.log('My number: ' + parseInt(process.argv[2]));
+} else {
+  console.log('Not a number');
 }
