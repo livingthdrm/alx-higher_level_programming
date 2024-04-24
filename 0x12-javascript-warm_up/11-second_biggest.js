e099a23cde3c12fe5fd201e3f8f981de95a28b41
livingthdrm <salaonjoseph@gmail.com>
2024-04-24 09:39:44 -0700

    second max

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/* a script that searches the
+ * second biggest integer in the list of arguments. */
+if (process.argv.length <= 3) {
+	console.log(0);
+}
+else {
+	const argument = [];
+	for (let i = 2; i <= process.argv.length; i++) {
+		argument.push(process.argv[i]);
+	}
+
+	argument.sort((a, b) => b - a);
+	console.log(argument[1]);
+}
