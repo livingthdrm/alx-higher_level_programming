c5b504f057dcc08f76cb53fc8af05f6e11f76802
livingthdrm <salaonjoseph@gmail.com>
2024-04-24 01:17:31 -0700

    factorial in javascript

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,18 @@
 #!/usr/bin/node
 /* a script that computes and prints a factorial */
 
-function factorial(a) {
-	let result = a;
-	if (isNaN(a)){
-		console.log('NaN');
-	}
-	else {
-		result *= factorial(a - 1);
-	}
-	return result;
+function factorial (a) {
+  if ((a === 1) || (a === 0)) {
+    return 1;
+  } else {
+    return (a * factorial(a - 1));
+  }
 }
 
-factorial(process.argv[2]);
+if (process.argv.length !== 3) {
+  console.log('NaN');
+} else if (parseInt(process.argv[2], 10)) {
+  console.log(factorial(process.argv[2]));
+} else {
+  console.log('NaN');
+}
