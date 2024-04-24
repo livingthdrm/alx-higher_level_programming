05999db4226f40df0500747580eb24ab43ca11f6
livingthdrm <salaonjoseph@gmail.com>
2024-04-24 09:43:07 -0700

    max and sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,14 +2,13 @@
 /* a script that searches the
  * second biggest integer in the list of arguments. */
 if (process.argv.length <= 3) {
-	console.log(0);
-}
-else {
-	const argument = [];
-	for (let i = 2; i <= process.argv.length; i++) {
-		argument.push(process.argv[i]);
-	}
+  console.log(0);
+} else {
+  const argument = [];
+  for (let i = 2; i <= process.argv.length; i++) {
+    argument.push(process.argv[i]);
+  }
 
-	argument.sort((a, b) => b - a);
-	console.log(argument[1]);
+  argument.sort((a, b) => b - a);
+  console.log(argument[1]);
 }
