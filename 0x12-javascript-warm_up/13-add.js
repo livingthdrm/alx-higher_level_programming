9a5666e631ee454a3e5b9850fc3f1cc6923b0d47
livingthdrm <salaonjoseph@gmail.com>
2024-04-26 06:41:38 -0700

    exporting named function

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* a function that returns the addition of 2 integers.
+ * The function must be visible from outside
+ * The name of the function must be add */
+
+exports.add = (x, y) => {
+  return x + y;
+};
