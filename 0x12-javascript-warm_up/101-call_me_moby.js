b605d09d5a05bc6d0003f0ab6391c3a3bf380178
livingthdrm <salaonjoseph@gmail.com>
2024-04-26 10:25:09 -0700

    call me moby

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* a function that executes x times a function.
+ * The function must be visible from outside
+ * Prototype: function (x, theFunction) */
+
+exports.callMeMoby = (x, theFunction) => {
+	for (let i = 0; i < x; i++){
+		theFunction();
+	}
+}
