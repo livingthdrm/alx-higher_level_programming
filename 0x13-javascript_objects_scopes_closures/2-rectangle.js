91b04cd1112732e10768cd61e1891716b407553f
livingthdrm <salaonjoseph@gmail.com>
2024-05-01 06:03:34 -0700

    fixing spaces and tabs

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,14 +3,13 @@
 
 class Rectangle {
   constructor (w, h) {
-	  if ((w > 0) && (h > 0)) {
-		  this.width = w;
-		  this.height = h;
-	  }
-	  else {
-		  this.width = undefined;
-		  this.height = undefined;
-	  }
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    } else {
+      this.width = undefined;
+      this.height = undefined;
+    }
   }
 }
 
