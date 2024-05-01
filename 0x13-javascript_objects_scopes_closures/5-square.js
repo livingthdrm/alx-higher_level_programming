5869ea5d5e0c54ee2287555e4b2b9625ce212212
livingthdrm <salaonjoseph@gmail.com>
2024-05-01 07:25:59 -0700

    using extends to inherit

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,42 @@
+#!/usr/bin/node
+/* a class Rectangle that defines a rectangle */
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      process.stdout.write('X');
+      for (let j = 1; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = (this.width * 2);
+    this.height = (this.height * 2);
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+}
+
+module.exports = Rectangle;
+module.exports = Square;
