9ef0a175b70c38f53ae53f10dfcb0c55fb583e93
livingthdrm <salaonjoseph@gmail.com>
2024-05-03 06:53:00 -0700

    tests failed

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,7 +12,7 @@ def add_integer(a, b=98):
     a and b must be first casted to integers if they are float """
     if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
-    if not isinstance(b, (int, float)):
+    elif not isinstance(b, (int, float)):
         raise TypeError('b must be an integer')
 
     return(int(a) + int(b))
