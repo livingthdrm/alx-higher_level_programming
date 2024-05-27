6737d7d72536b8c5c0775f30a9fe1fab7d9c91dd
livingthdrm <salaonjoseph@gmail.com>
2024-05-27 15:07:46 -0700

    fix elif statements

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -12,10 +12,10 @@ def print_square(size):
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-    if isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
-    if size < 0:
+    elif size < 0:
         raise ValueError("size must be >= 0")
+    elif isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
 
     for _ in range(size):
         print("#" * size)
