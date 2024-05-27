37d1089e9a2e43d41f5aac9f746ac47599c4cec1
livingthdrm <salaonjoseph@gmail.com>
2024-05-27 15:05:45 -0700

    fix print squares test case

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -10,14 +10,12 @@ def print_square(size):
     """
     size must be an integer
     """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
 
-    for length in range(size):
-        for width in range(size - 1):
-            print('#', end='')
-        print('#')
+    for _ in range(size):
+        print("#" * size)
